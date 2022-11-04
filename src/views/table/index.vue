cbddb865e228c29afd6fd185abec1eaa335457a8
YOKI1216 <1151509140@qq.com>
2022-11-04 21:13:42 +0800

    修改了操作表单多余部分

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,78 +1,52 @@
 
 <template>
   <div class="user-main">
-    <el-button>添加</el-button>
+    <el-button @click="controller">添加</el-button>
     <!-- slot-scope="scope " 来 取得 作用域插槽 :data绑定的数据 -->
     <el-table v-loading="listLoading" :data="user" border style="width: 100%">
 
       <el-table-column fixed prop="user_id" label="ID" width="150" />
       <el-table-column fixed prop="user_name" label="姓名" width="150" />
       <el-table-column fixed prop="user_sex" label="性别" width="150" />
-      <el-table-column fixed prop="user_age" label="电话" width="150" />
-      <el-table-column fixed prop="user_tel" label="薪资" width="150" />
-      <el-table-column fixed prop="user_salary" label="日期" width="150" />
+      <el-table-column fixed prop="user_age" label="年龄" width="150" />
+      <el-table-column fixed prop="user_tel" label="电话" width="150" />
+      <el-table-column fixed prop="user_salary" label="薪水" width="150" />
       <el-table-column fixed prop="user_password" label="登录密码" width="150" />
-      <el-table-column fixed="right" label="操作" width="100">
+      <!-- 操作 -->
+      <el-table-column fixed="right" label="操作" width="150">
         <template slot-scope="scope">
-          <el-button type="text" size="small" @click="handleClick(scope.row)">查看</el-button>
-          <el-button type="text" size="small" @click="modify">编辑</el-button>
+          <!-- 查看这行数据 -->
+          <el-button type="text" size="small" @click="controller">查看</el-button>
+          <!-- 修改 -->
+          <el-button type="text" size="small" @click="controller">编辑</el-button>
+          <!-- 删除 -->
+          <el-button type="text" size="small" @click="controller">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
 
-    <el-button @click="open">点我打开</el-button>
+    <!-- 操作弹窗 -->
     <Dialog ref="user" v-bind="user" :config="config" :before-close="beforeClose" @close="resetForm">
-      <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="100px" class="demo-ruleForm">
-        <el-form-item label="活动名称" prop="name">
-          <el-input v-model="ruleForm.name" />
+      <el-form ref="userForm" :model="userForm" :rules="userRules" label-width="100px">
+        <el-form-item label="姓名" prop="user_id">
+          <el-input v-model="userForm.user_id" />
         </el-form-item>
-        <el-form-item label="活动区域" prop="region">
-          <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
-            <el-option label="区域一" value="shanghai" />
-            <el-option label="区域二" value="beijing" />
-          </el-select>
+        <el-form-item label="性别" prop="user_sex">
+          <el-input v-model="userForm.user_sex" />
         </el-form-item>
-        <el-form-item label="活动时间" required>
-          <el-col :span="11">
-            <el-form-item prop="date1">
-              <el-date-picker v-model="ruleForm.date1" type="date" placeholder="选择日期" style="width: 100%;" />
-            </el-form-item>
-          </el-col>
-          <el-col class="line" :span="2">-</el-col>
-          <el-col :span="11">
-            <el-form-item prop="date2">
-              <el-time-picker v-model="ruleForm.date2" placeholder="选择时间" style="width: 100%;" />
-            </el-form-item>
-          </el-col>
+        <el-form-item label="年龄" prop="user_age">
+          <el-input v-model="userForm.user_age" />
         </el-form-item>
-        <el-form-item label="即时配送" prop="delivery">
-          <el-switch v-model="ruleForm.delivery" />
+        <el-form-item label="电话" prop="user_tel">
+          <el-input v-model="userForm.user_tel" />
         </el-form-item>
-        <el-form-item label="活动性质" prop="type">
-          <el-checkbox-group v-model="ruleForm.type">
-            <el-checkbox label="美食/餐厅线上活动" name="type" />
-            <el-checkbox label="地推活动" name="type" />
-            <el-checkbox label="线下主题活动" name="type" />
-            <el-checkbox label="单纯品牌曝光" name="type" />
-          </el-checkbox-group>
+        <el-form-item label="薪水" prop="user_salary">
+          <el-input v-model="userForm.user_salary" />
         </el-form-item>
-        <el-form-item label="特殊资源" prop="resource">
-          <el-radio-group v-model="ruleForm.resource">
-            <el-radio label="线上品牌商赞助" />
-            <el-radio label="线下场地免费" />
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="活动形式" prop="desc">
-          <el-input v-model="ruleForm.desc" type="textarea" />
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        <el-form-item label="密码" prop="user_password">
+          <el-input v-model="userForm.user_password" />
         </el-form-item>
       </el-form>
-    </Dialog>
-    <Dialog ref="dialog" :config="config" :before-close="beforeClose" @close="resetForm">
-      <span>123</span>
     </Dialog>
   </div>
 
@@ -97,10 +71,18 @@ export default {
   },
   data() {
     return {
-      list: null,
+      // 所有用户对象
       user: [{
-
+        name: '',
+        user_id: '',
+        user_name: '',
+        user_sex: '',
+        user_age: '',
+        user_tel: '',
+        user_salary: '',
+        user_password: ''
       }],
+      // 信息加载开关
       listLoading: true,
       config: {
         top: '10vh',
@@ -109,39 +91,35 @@ export default {
         center: true,
         btnTxt: ['取消', '提交']
       },
-
-      ruleForm: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+      // 用户表单
+      userForm: {
+        user_id: '',
+        user_name: '',
+        user_sex: '',
+        user_age: '',
+        user_tel: '',
+        user_salary: '',
+        user_password: ''
       },
-      rules: {
-        name: [
+      userRules: {
+        user_id: [
           { required: true, message: '请输入活动名称', trigger: 'blur' },
           { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
         ],
-        region: [
-          { required: true, message: '请选择活动区域', trigger: 'change' }
+        user_sex: [
+          { required: true, message: '请输入性别', trigger: 'blur' }
         ],
-        date1: [
-          { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+        user_age: [
+          { required: true, message: '请输入年龄', trigger: 'blur' }
         ],
-        date2: [
-          { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
+        user_tel: [
+          { required: true, message: '请输入电话', trigger: 'blur' }
         ],
-        type: [
-          { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
+        user_salary: [
+          { required: true, message: '请输入薪水', trigger: 'blur' }
         ],
-        resource: [
-          { required: true, message: '请选择活动资源', trigger: 'change' }
-        ],
-        desc: [
-          { required: true, message: '请填写活动形式', trigger: 'blur' }
+        user_password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
         ]
       }
       // 尾
@@ -158,20 +136,12 @@ export default {
     fetchData() {
       this.listLoading = true
       getUser().then(response => {
-        this.list = response.data
         this.user = response.data.data
         this.listLoading = false
       })
     },
-    open() {
-      // 这里就充分利用了open方法中返回的nextTick
-      this.$refs.dialog.open(cancel => {
-        // cancel();
-        console.log('点击提交按钮了')
-      })
-        .then(() => { console.log(this.$refs.span) })
-    },
-    modify() {
+    // 启动弹窗
+    controller() {
       this.$refs.user.open(
         cancel => {
         // cancel();
