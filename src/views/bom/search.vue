b319ce0b3eccde07c27f15d2a59bc1913f0e5fee
YOKI1216 <1151509140@qq.com>
2024-03-23 16:40:22 +0800

    显示bug修复，添加bug修复

--- src/views/bom/search.vue
+++ src/views/bom/search.vue
@@ -13,7 +13,7 @@
         <el-button @click="openMaterialInfoFromWindows">产品物料信息添加</el-button>
       </el-col>
     </el-row>
-    <el-table :data="productList" border style="width: 100%">
+    <el-table :data="[product]" border style="width: 100%">
       <el-table-column fixed label="产品名称" prop="product_name" />
       <el-table-column fixed label="产品描述" prop="description" />
       <el-table-column fixed label="产品价格" prop="price" />
@@ -22,17 +22,23 @@
     <AddMaterialInfo ref="openDialog" :before-close="beforeClose" :config="config" @close="resetForm">
       <el-form label-width="100px">
         <el-form-item label="产品名称" prop="">
-          <h1 v-for="product in productList" :key="product.product_id">{{ product.product_name }}</h1>
+          <h1>{{ product.product_name }}</h1>
         </el-form-item>
         <el-form-item label="物料名称" prop="">
-          <el-select v-model="selectedMaterial" placeholder="请选择">
+          <el-select v-model="BOM_item.material_id" placeholder="请选择需要添加的物料">
             <el-option
               v-for="item in material_list"
               :key="item.material.material_name"
               :label="item.material.material_name"
-              :value="item.material.material_name"
+              :value="item.material.material_id"
             />
           </el-select>
+        </el-form-item>
+        <el-form-item label="数量" prop="">
+          <el-input-number v-model="BOM_item.quantity" />
+        </el-form-item>
+        <el-form-item label="单位" prop="">
+          <el-input v-model="BOM_item.unit" size="mini" style="width: 100px;" />
         </el-form-item>
         <el-form-item label="操作">
           <el-button @click="add()">添加</el-button>
@@ -43,9 +49,9 @@
 </template>
 
 <script>
-import { getAllBom } from '@/api/bom'
-import { getAllMaterial } from '@/api/material'
+import { addBom, getAllBom } from '@/api/bom'
 import AddMaterialInfo from './AddMaterialInfo.vue'
+import { getAllMaterial } from '@/api/material'
 export default {
   components: {
     AddMaterialInfo
@@ -54,9 +60,16 @@ export default {
     return {
       search: '', // 当前输入框的值
       searchList: ['暂无数据'],
-      productList: [],
+      product: {},
       materialList: [],
-      selectedMaterial: null,
+      BOM_item: {
+        bom_id: null,
+        product_name: '',
+        material_id: null,
+        quantity: 0,
+        unit: '',
+        is_deleted: 0
+      },
       material_list: [], // 物料选择下拉框数据
       config: {
         top: '10vh',
@@ -88,12 +101,12 @@ export default {
         if (response.code === 20041) {
           this.$message.success('获取所有物料信息成功')
           // 清空信息
-          this.productList = []
+          this.product = {}
           this.materialList = []
           this.searchList = response.data
           this.searchList.forEach(item => {
             if (item.product) {
-              this.productList.push(item.product)
+              this.product = item.product
             } else {
               this.materialList.push(item)
             }
@@ -107,6 +120,7 @@ export default {
     // 添加物料信息
     openMaterialInfoFromWindows() {
       this.$message.success('窗口打开成功')
+      // 刷新操作
       getAllMaterial().then(response => {
         this.material_list = response.data
       })
@@ -122,6 +136,21 @@ export default {
     },
     resetForm(formName) {
       this.$refs[formName].resetFields()
+    },
+    add() {
+      // 关闭弹窗
+      this.$refs.openDialog.cancel()
+      this.BOM_item.product_name = this.product.product_name
+      addBom(this.BOM_item).then(response => {
+        // 触发父组件更新数据
+        this.$emit('add')
+        if (response.code === 20011) {
+          this.$message.success('物料添加成功')
+          this.searchHandler()
+        } else {
+          this.$message.error('物料添加失败')
+        }
+      })
     }
   }
 }
